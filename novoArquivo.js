7830afd14a841e8a53fa34271dca57fd13726755
alebitencourt <ale.bite@hotmail.com>
2022-05-16 22:12:08 -0300

    aulaHoje2

---
+++ novoArquivo.js
@@ -0,0 +1,12 @@
+let nomeDoUsuario = "João";
+
+let idade = 20;
+
+let ano = 2022;
+
+// Utilizando operador(es) matemático(s) e o console.log(), retorna a seguinte mensagem no terminal:
+
+// Olá João, você nasceu em XXXX.
+
+
+console.log('Olá '+nomeDoUsuario+','+' '+'você nasceu em '+(+ano-idade));
